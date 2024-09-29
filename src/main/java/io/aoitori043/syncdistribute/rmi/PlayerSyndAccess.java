0eca9274340fd96395d27e93a6cdad5f01cb1c70
tsunami047 <sussicran@proton.me>
2024-09-30 00:24:04 +0800

    (Added data access classes and persistence manager for better data management)

--- src/main/java/io/aoitori043/syncdistribute/rmi/PlayerSyndAccess.java
+++ src/main/java/io/aoitori043/syncdistribute/rmi/PlayerSyndAccess.java
@@ -1,9 +1,17 @@
 package io.aoitori043.syncdistribute.rmi;
 
+import io.aoitori043.syncdistribute.rmi.data.PersistentDataAccess;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @Author: natsumi
  * @CreateTime: 2024-09-30  00:21
  * @Description: ?
  */
 public class PlayerSyndAccess {
+
+    public static ConcurrentHashMap<String, PersistentDataAccess> persistentMap = new ConcurrentHashMap<>();
+
+
 }
