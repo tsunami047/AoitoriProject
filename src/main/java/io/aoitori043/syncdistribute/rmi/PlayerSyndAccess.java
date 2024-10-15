470d69f9e30e917b6af09237e1eb292ea7897964
aoitori043 <sussicran@proton.me>
2024-10-15 10:47:32 +0800

    Added data access classes and persistence manager

--- src/main/java/io/aoitori043/syncdistribute/rmi/PlayerSyndAccess.java
+++ src/main/java/io/aoitori043/syncdistribute/rmi/PlayerSyndAccess.java
@@ -9,14 +9,20 @@ import java.util.concurrent.ConcurrentHashMap;
  * @CreateTime: 2024-09-30  00:21
  * @Description: ?
  */
+//TODO 完善直接取功能
 public class PlayerSyndAccess {
 
     public static ConcurrentHashMap<String, PersistentDataAccess> persistentMap = new ConcurrentHashMap<>();
 
+    public static PersistentDataAccess createPersistentDataAccess(String playerName){
+        return new PersistentDataAccess(playerName);
+    }
+
     public static PersistentDataAccess getPersistentDataAccess(String playerName){
-        return persistentMap.computeIfAbsent(playerName, k -> {
-            return new PersistentDataAccess(playerName);
-        });
+        return persistentMap.get(playerName);
+//        return persistentMap.computeIfAbsent(playerName, k -> {
+//            return new PersistentDataAccess(playerName);
+//        });
     }
 
 
